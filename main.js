5da5759a9db65266b5dfac54f9ac85d1b59c9619
ligmasandro <sandrokashiashvili1@Gmail.com>
2025-05-02 12:35:04 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const img1 = document.querySelector(".sun")
+const elem1 = document.querySelector("body")
+const elem2 = document.querySelector(".main-container")
+img1.addEventListener("click",()=>{
+    elem1.style.backgroundImage = "url(icon-moon.svg)";
+
+
+
+})
